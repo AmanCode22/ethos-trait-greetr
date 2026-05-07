1a98aea9ac360aa92c5c9f6799be69387bcc30ca
Aman Adlakha <amanady125@gmail.com>
2026-05-07 18:56:39 +0530

    Update trait.rs

--- src/trait.rs
+++ src/trait.rs
@@ -2,17 +2,20 @@ use core::ffi::c_char;
 use core::slice;
 use core::str;
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn greet(name: *mut c_char) {
     if name.is_null() { return; }
+
     let mut len = 0;
-    while *name.offset(len) != 0 {
-        len += 1;
-    }
+    unsafe {
+        while *name.offset(len) != 0 {
+            len += 1;
+        }
 
-    let bytes = slice::from_raw_parts(name as *const u8, len as usize);
+        let bytes = slice::from_raw_parts(name as *const u8, len as usize);
 
-    if let Ok(name_str) = str::from_utf8(bytes) {
-        println!("Hello, {}", name_str);
+        if let Ok(name_str) = str::from_utf8(bytes) {
+            println!("Hello, {}", name_str);
+        }
     }
 }
